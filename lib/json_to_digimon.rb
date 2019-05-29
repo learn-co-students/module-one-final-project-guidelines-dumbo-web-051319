093c27b4138a39fd7c8a033be97ba16d8bf26c9c
Tess Neau <tessneau@gmail.com>
2019-05-29 14:14:03 -0400

    test

--- lib/json_to_digimon.rb
+++ lib/json_to_digimon.rb
@@ -3,40 +3,43 @@ require 'pry'
 
 ## only uses digimons data from 1- 120
 
-
-## READING AND PARSING EVOLUTION JSON FILE (JUST INCLUDES 0001 - 0120, I ADDED THE TREE-PATCH FILE MANUALLY)
+def evolution_data
+## reads evolution json (1-120 digimons) and parses
 evo_file = File.read("#{__dir__}/../dh-data/json/tree-0002-0120.json")
 evo_data = JSON.parse(evo_file)
-
-
-## READING AND PARSING STATS LIST JSON FILE (JUST INCLUDES 0001 - 0120)
+end
+#
+def stats_data
+## reads stats json (1-120 digimons) and parses
 list_file = File.read("#{__dir__}/../dh-data/json/list-0001-0120.json")
 list_data = JSON.parse(list_file)
-
-
-## CREATING A NEW HASH TO TAKE ONLY THE DATA WE WANT FROM PARSED EVOLUTION HASH
-digi_evo = {}
-
-evo_data.each do |key,value|
-  if evo_data[key].include?("evolution")
-  digi_evo[key.to_i] = {
-    has_evo: value["has_evolution_tree"],
-    evolution_id: value["evolution"].keys[-1].to_i
-  }
-  else
-  digi_evo[key.to_i] = {
-    has_evo: value["has_evolution_tree"],
-    evolution_id: []
-  }
-  end
 end
+
+def get_digimons
+
+  ## CREATING A NEW HASH TO TAKE ONLY THE DATA WE WANT FROM PARSED EVOLUTION HASH
+  digi_evo = {}
+
+  evolution_data.each do |key,value|
+    if evolution_data[key].include?("evolution")
+    digi_evo[key.to_i] = {
+      has_evo: value["has_evolution_tree"],
+      evolution_id: value["evolution"].keys[-1].to_i
+    }
+    else
+    digi_evo[key.to_i] = {
+      has_evo: value["has_evolution_tree"],
+      evolution_id: 0
+    }
+    end
+  end
 
 ## CREATING A NEW HASH TO TAKE ONLY THE DATA WE WANT FROM PARSED STATS LIST HASH
 ## digi_hash IS THE FINAL HASH
 
 digi_hash = {}
 
-list_data.each do |key, value|
+stats_data.each do |key, value|
   digi_hash[key.to_i] = {
     name: value["name"],
     race: value["main_race"],
@@ -66,20 +69,26 @@ end
       end
     end
   end
+digi_hash
+end
+
+# data = get_digimons
+#
+# binding.pry
+#
+# puts 1
 
 
   #### DETERMINING WHAT KIND OF TYPES THERE ARE:
   #### DRAGON, KNIGHT, NATURE, DARK, AND HOLY
 
-type_array = []
-
-  digi_hash.each do |key, value|
-    type_array << value[:race]
-  end
-
-  type_array.uniq
-
-binding.pry
+# type_array = []
+#
+#   digi_hash.each do |key, value|
+#     type_array << value[:race]
+#   end
+#
+#   type_array.uniq
 
 
 # IF I EVER WANT TO ADD SEVERAL FILES USE THIS
@@ -92,7 +101,3 @@ binding.pry
 #
 # data.keys.map {|string_id| string_id.to_i}
 #
-
-
-
-puts 1
